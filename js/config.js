a66a75661d0946e478e0b90257b02fb0d0191df8
Osman Emre <osmanemreercin@gmail.com>
2015-11-22 23:47:02 +0200

    First commit.

---
+++ js/config.js
@@ -0,0 +1,99 @@
+materialAdmin
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/home");
+
+
+        $stateProvider
+
+        //------------------------------
+        // HOME
+        //------------------------------
+
+            .state ('home', {
+                url: '/home',
+                templateUrl: 'views/home.html',
+                resolve: {
+                    loadPlugin: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load ([
+                            {
+                                name: 'css',
+                                insertBefore: '#app-level',
+                                files: [
+                                    'vendors/bower_components/fullcalendar/dist/fullcalendar.min.css'
+                                ]
+                            },
+                            {
+                                name: 'vendors',
+                                insertBefore: '#app-level-js',
+                                files: [
+                                    'vendors/sparklines/jquery.sparkline.min.js',
+                                    'vendors/bower_components/jquery.easy-pie-chart/dist/jquery.easypiechart.min.js',
+                                    'vendors/bower_components/simpleWeather/jquery.simpleWeather.min.js'
+                                ]
+                            }
+                        ])
+                    }
+                }
+            })
+
+            //------------------------------
+            // PAGES
+            //------------------------------
+
+            .state ('pages', {
+                url: '/pages',
+                templateUrl: 'views/common.html'
+            })
+
+
+            //Profile
+
+            .state ('pages.profile', {
+                url: '/profile',
+                templateUrl: 'views/profile.html'
+            })
+
+            .state ('pages.profile.profile-about', {
+                url: '/profile-about',
+                templateUrl: 'views/profile-about.html'
+            })
+
+
+            // Polls
+
+            .state ('pages.poll', {
+                url: '/poll',
+                templateUrl: 'views/poll.html'
+            })
+
+            .state ('pages.poll.poll-questions', {
+                url: '/poll-questions',
+                templateUrl: 'views/poll-questions.html',
+                resolve: {
+                    loadPlugin: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load ([])
+                    }
+                }
+            })
+
+            .state ('pages.poll.poll-results', {
+                url: '/poll-results',
+                templateUrl: 'views/poll-results.html',
+                resolve: {
+                    loadPlugin: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load ([])
+                    }
+                }
+            })
+
+            .state ('pages.poll.poll-create-new', {
+                url: '/poll-create-new',
+                templateUrl: 'views/poll-create-new.html',
+                resolve: {
+                    loadPlugin: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load ([])
+                    }
+                }
+            })
+
+    });
