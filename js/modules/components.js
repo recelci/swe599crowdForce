27cc19e224b4535cc4f4a93d3002a060656d1d0f
Osman Emre <osmanemreercin@gmail.com>
2015-12-28 17:25:11 +0200

    Bug fixes and extra functional changes.

--- js/modules/components.js
+++ js/modules/components.js
@@ -85,15 +85,15 @@ materialAdmin
             link: function(scope, element, attrs) {
                 element.click(function(){
                     swal({   
-                        title: "Are you sure?",   
-                        text: "You will not be able to recover this imaginary file!",   
+                        title: "Are you sure to delete this?",
+                        text: "You will not be able to recover this template!",
                         type: "warning",   
                         showCancelButton: true,   
                         confirmButtonColor: "#DD6B55",   
                         confirmButtonText: "Yes, delete it!",   
                         closeOnConfirm: false 
                     }, function(){   
-                        swal("Deleted!", "Your imaginary file has been deleted.", "success"); 
+                        swal("Deleted!", "Your template has been deleted.", "success");
                     });
                 });
             }
